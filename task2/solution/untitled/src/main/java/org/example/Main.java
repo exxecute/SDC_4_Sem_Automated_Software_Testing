5bb1ca0202e1dbd03445b1027b3941d14208d88e
execute <3xecvte@gmail.com>
2025-05-07 18:43:55 +0300

    feat: task2 add logic in main

--- task2/solution/untitled/src/main/java/org/example/Main.java
+++ task2/solution/untitled/src/main/java/org/example/Main.java
@@ -2,12 +2,23 @@ package org.example;
 
 import org.example.circle.Circle;
 
+import java.util.Scanner;
+
 public class Main {
     private static final String APPLICATION_HEADER = "TASK 2 Automated Software Testing Java, Uladzislau Mikhayevich JA2";
     public static void main(String[] args) {
         System.out.println(APPLICATION_HEADER);
-        Circle c1 = new Circle(10, 10);
-        Circle c2 = new Circle(20, 1);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter x1 r1 x2 r2 (separated by space):");
+        int x1 = scanner.nextInt();
+        int r1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int r2 = scanner.nextInt();
+        scanner.close();
+
+        Circle c1 = new Circle(x1, r1);
+        Circle c2 = new Circle(x2, r2);
 
         System.out.println(c1.getIntersections(c2));
     }
