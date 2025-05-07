2a3c723b183f16144339ba0aba601c43c887c262
execute <3xecvte@gmail.com>
2025-05-07 18:29:46 +0300

    test: task2 do test

---
+++ task2/solution/untitled/src/test/java/org/example/circle/CircleTest.java
@@ -0,0 +1,25 @@
+package org.example.circle;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @ParameterizedTest(name = "x1={0}, r1={1}, x2={2}, r2={3} => expected: {4}")
+    @CsvSource({
+            "0, 3, 10, 3, NO_INTERSECTIONS",
+            "0, 6,  0, 3, NO_INTERSECTIONS",
+            "0, 3,  6, 3, ONE_INTERSECTION",
+            "0, 3,  2, 1, ONE_INTERSECTION",
+            "0, 3,  3, 2, TWO_INTERSECTIONS",
+            "0, 3,  0, 3, ALL_INTERSECTIONS",
+    })
+    void getIntersectionsTest(int x1, int r1, int x2, int r2, Intersections expected) {
+        Circle c1 = new Circle(x1, r1);
+        Circle c2 = new Circle(x2, r2);
+        assertEquals(expected, c1.getIntersections(c2));
+    }
+}
