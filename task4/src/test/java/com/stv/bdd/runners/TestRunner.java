3f35b3a2b25fb1075f055162a208b865fd90df2c
Uladzislau Mikhayevich <uladzislau.mikhayevich@student.ehu.lt>
2025-06-06 20:54:24 +0300

    feat: brute force after class close

--- task4/src/test/java/com/stv/bdd/runners/TestRunner.java
+++ task4/src/test/java/com/stv/bdd/runners/TestRunner.java
@@ -1,7 +1,9 @@
 package com.stv.bdd.runners;
 
+import com.stv.framework.core.drivers.MyDriver;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.FileInputStream;
@@ -32,5 +34,10 @@ public class TestRunner extends AbstractTestNGCucumberTests {
         } else {
             System.out.println("[INFO] Base URL provided via system property: " + System.getProperty("base.url"));
         }
+    }
+
+    @AfterClass
+    public void afterClass() throws Exception {
+        MyDriver.getDriver().quit();
     }
 }
