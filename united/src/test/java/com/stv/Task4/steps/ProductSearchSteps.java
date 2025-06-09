741472eff9e2f4e99c8975d49ff1cae56c33d627
Uladzislau Mikhayevich <uladzislau.mikhayevich@student.ehu.lt>
2025-06-09 19:58:38 +0300

    feat: unite projects copy to united projects

---
+++ united/src/test/java/com/stv/Task4/steps/ProductSearchSteps.java
@@ -0,0 +1,29 @@
+package com.stv.bdd.steps;
+
+import com.stv.factory.factorypages.HomePage;
+import com.stv.factory.factorypages.ProductListPage;
+import io.cucumber.java.en.*;
+import org.testng.Assert;
+
+public class ProductSearchSteps {
+    private ProductListPage productListPage;
+    private HomePage homePage;
+
+    @Given("the user is on the homepage")
+    public void theUserIsOnTheHomepage() {
+        this.homePage = new HomePage();
+        this.homePage.acceptCookiesIfPresent();
+    }
+
+    @When("the user searches for {string}")
+    public void theUserSearchesFor(String searchTerm) {
+        this.homePage.getSearchBox(searchTerm);
+        this.productListPage = new ProductListPage();
+        this.productListPage.setElement(searchTerm);
+    }
+
+    @Then("the product list should contain relevant items")
+    public void theProductListShouldContainRelevantItems() {
+        Assert.assertTrue(this.productListPage.isRelevantItems(), "Not contains relevant items");
+    }
+}
